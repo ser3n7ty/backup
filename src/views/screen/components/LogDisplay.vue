4a22b07b60a91ceb34f641dc2550656ab4eaa866
You Tingwang <youngtingwang@outlook.com>
2023-11-30 18:50:12 +0800

    feat(nothing): backup

--- src/views/screen/components/LogDisplay.vue
+++ src/views/screen/components/LogDisplay.vue
@@ -1,169 +1,97 @@
 <template>
   <div class="container">
     <div class="header">
-      <table>
-        <thead>
-          <th>访问时间</th>
-          <th>源IP</th>
-          <th>处理模式</th>
-          <th>处理结果</th>
-        </thead>
-      </table>
+      <h4>访问时间——源IP——处理模式——响应结果</h4>
     </div>
-    <br>
-    <div class="display">
-      <vue-seamless-scroll
-        ref="seamlessScroll"
-        :data="listData"
-        :class-option="classOption"
-        class="warp"
-      >
-        <ul class="item">
-          <li v-for="(item, index) in listData" :key="index" :class="{ 'intercepted': item.result === '1' }">
-            <span class="communal" v-text="item.time" />
-            <span class="communal" v-text="item.ip" />
-            <span class="communal" v-text="getPattern(item.pattern)" />
-            <span class="communal" v-text="getResult(item.result)" />
-          </li>
-        </ul>
-      </vue-seamless-scroll>
+    <div ref="logContainer" class="log-container">
+      <ul>
+        <li v-for="(log, index) in visibleLogs" :key="index">{{ log }}</li>
+      </ul>
     </div>
   </div>
+
 </template>
 
 <script>
-import vueSeamlessScroll from 'vue-seamless-scroll'
-
 export default {
-  name: 'Example10Basic',
-  components: {
-    vueSeamlessScroll
-  },
+  // TODO:增加事件监听，支持用户手动滚动。当无动作5秒后自动滚动到末尾
   data() {
     return {
-      interval: null,
-      classOption: {
-        step: 0.1,
-        waitTime: 2000,
-        hoverStop: true,
-        direction: 1,
-        limitMoveNum: 6,
-        singleHeight: 30
-      },
-      listData: [
-        {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }, {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '1'
-        }]
+      logs: [], // 存储日志信息的数组
+      visibleLogs: [], // 存储可见的日志信息的数组
+      timer: null, // 定时器用于模拟获取数据
+      containerHeight: 300 // 容器高度
     }
   },
   mounted() {
-    this.interval = setInterval(() => {
-      console.log('追加数据')
-      const data = [
-        {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '0'
-        },
-        {
-          'time': '2023-12-12 15:45',
-          'ip': '192.65.12.122',
-          pattern: 0,
-          result: '0'
-        }
-      ]
-      // this.fetchLogs()
-      // this.listData = this.listData.concat(this.fetchData)
-      this.listData = this.listData.concat(data)
-      this.$refs.seamlessScroll.reset()
-    }, 5000)
+    this.startFetchingLogs()
   },
   beforeDestroy() {
-    clearInterval(this.interval)
+    this.stopFetchingLogs()
   },
   methods: {
-    getPattern(pattern) {
-      return pattern === 0 ? '并行' : '串行'
+    async startFetchingLogs() {
+      this.timer = setInterval(() => {
+        this.fetchLogs()
+      }, 1000)
     },
-    getResult(result) {
-      return result === '0' ? '放行' : '拦截'
+    stopFetchingLogs() {
+      clearInterval(this.timer)
+    },
+    async fetchLogs() {
+      const currentTime = new Date().toLocaleTimeString()
+      const sourceIP = '192.168.1.1' // 替换为实际获取的源IP
+      const processingMode = 'Mode A' // 替换为实际获取的处理模式
+      const response = 'Success' // 替换为实际获取的响应结果
+      this.updateLogs(`${currentTime} - ${sourceIP} - ${processingMode} - ${response}`)
+      // try {
+      //   const data = await this.$store.dispatch('screen/getLogs')
+      //   this.updateLogs(data)
+      // } catch (error) {
+      //   this.$message({
+      //     message: 'Failed to fetch logs' + ',' + error,
+      //     type: 'error'
+      //   })
+      // }
+    },
+    updateLogs(newLogs) {
+      this.logs = this.logs.concat(newLogs)
+
+      // 只显示最后容器高度可容纳的部分日志
+      const startIndex = Math.max(this.logs.length - this.containerHeight / 20, 0)
+      this.visibleLogs = this.logs.slice(startIndex)
+
+      this.scrollLogContainerToBottom()
+    },
+    scrollLogContainerToBottom() {
+      // 仅当滚动条在底部时才自动滚动
+      const logContainer = this.$refs.logContainer
+      const isScrolledToBottom = logContainer.scrollHeight - logContainer.clientHeight <= logContainer.scrollTop + 1
+      if (isScrolledToBottom) {
+        logContainer.scrollTop = logContainer.scrollHeight - logContainer.clientHeight
+      }
     }
-    // fetchLogs() {
-    //   this.$store.dispatch('screen/getLogs')
-    //     .then(data => {
-    //       this.fetchData = data
-    //     })
-    // }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  .warp {
-    height: 270px;
-    width: 360px;
-    margin: 0 auto;
-    overflow: hidden;
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0 auto;
-      li,
-      a {
-        display: block;
-        height: 30px;
-        line-height: 30px;
-        display: flex;
-        justify-content: space-between;
-        font-size: 15px;
-        &.intercepted {
-          color: red;
-        }
-      }
-    }
-  }
+<style>
+.log-container {
+  height: 285px; /* 设置日志容器的高度 */
+  overflow-x: hidden; /* 隐藏横向滚动条 */
+  border: 1px solid #ccc; /* 边框样式 */
+  padding: 10px; /* 内边距 */
+}
+
+.log-container ul {
+  list-style: none; /* 去除列表样式 */
+  border: none;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif; /* 字体样式 */
+}
+
+.log-container ul li {
+  margin-bottom: 5px; /* 日志项之间的间距 */
+}
 </style>
