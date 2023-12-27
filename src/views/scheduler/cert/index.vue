be3393b7eda56911d1eef921bb091dee4be73c1a
You Tingwang <youngtingwang@outlook.com>
2023-12-27 15:53:17 +0800

    chore(icons): change the style of icons

---
+++ src/views/scheduler/cert/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>cert</span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/scheduler/list/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>list</span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
